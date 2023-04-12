378ceae5b0105f5ab8a1b1cd0b58d5ba4dda65ed
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-12 16:54:15 +0200

    Import port from .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 
+import { config } from 'dotenv'
+config()
+
 const app = express();
 
 app.get('/', (req, res, next) => {
     res.send('Hi :)');
 });
 
-app.listen(3000, () => {
-    console.log(`Express started on port 3000`)
+
+const PORT: Number = Number(process.env.PORT) || 3000
+
+app.listen(PORT, () => {
+    console.log(`Express started on port ${PORT}`)
 });

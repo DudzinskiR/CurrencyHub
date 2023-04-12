1c260502413a35896e04ff98ec38a4df870aa0cd
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-12 17:03:17 +0200

    Add http error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { Server } from "http";
+import createHttpError from "http-errors";
 
 import { config } from 'dotenv'
-import { Server } from "http";
 config()
 
 const app: Application = express();
@@ -10,6 +11,19 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hi :)');
 });
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(new createHttpError.NotFound());
+})
+
+const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500)
+    res.send({
+        status: err.status || 500,
+        message: err.message
+    })
+}
+
+app.use(errorHandler)
 
 const PORT: Number = Number(process.env.PORT) || 3000
 

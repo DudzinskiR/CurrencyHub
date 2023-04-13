bb8d6b8ee0ff0abcfc0846bbc53597c9c2c1ad33
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-13 19:48:05 +0200

    Add props

--- frontend/src/components/CountryFlag/CountryFlag.tsx
+++ frontend/src/components/CountryFlag/CountryFlag.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import "./CountryFlag"
 
-const CountryFlag = () => {
+interface props {
+    countryCode?: string,
+    size?: Number
+}
+
+const CountryFlag = ({countryCode = "US", size = 40}: props) => {
   return (
     <div>CountryFlag</div>
   )

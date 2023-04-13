cd4784a304bf449a4525db889edfa38703815c71
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-13 19:43:41 +0200

    Add div

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Home.scss"
 
+import CurrencyPicker from '../../components/CurrencyPicker/CurrencyPicker'
+
 const Home = () => {
   return (
-    <></>
+    <CurrencyPicker/>
   )
 }
 

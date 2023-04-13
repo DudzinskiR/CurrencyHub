c960a2359fc851e64f8f431b97f9c85cb8d1803e
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-13 18:48:55 +0200

    Clear home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import "./Home.scss"
-import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
-    <Header text="sszxczxczcsds"/>
+    <></>
   )
 }
 

b58d78071d99688bae0fcebc5d652427c6cd10ae
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-13 19:58:58 +0200

    Remove test file

--- frontend/src/components/CountryFlag/CountryFlag.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-
-test('placeholder', () => {
-  render(<div>a</div>);
-  const element = screen.getByText(/a/i);
-  expect(element).toBeInTheDocument();
-});

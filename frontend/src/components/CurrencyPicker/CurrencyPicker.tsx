aa090a8e77d087fa401074bfb03bf176393b2a0b
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-04-13 20:14:41 +0200

    Add CountryFlag

--- frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
+++ frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./CurrencyPicker"
+import CountryFlag from '../CountryFlag/CountryFlag'
 
 const CurrencyPicker = () => {
   return (
     <div className='currency-picker-box'>
         <button>
-            
+            <div className="flag"><CountryFlag countryCode='GB' size={40}/></div>
         </button>
     </div>
   )

93432ce7d4391e59b9639d50f873e804c1acb531
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-03 21:58:02 +0200

    Show new currency after change

--- frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
+++ frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./CurrencyPicker.scss"
 import CountryFlag from '../CountryFlag/CountryFlag'
 import { Currency } from '../../util/CurrencyData'
@@ -10,19 +10,27 @@ interface props{
 }
 
 const CurrencyPicker = (props: props) => {
+  const [selectedCurrency, setCurrency] = useState<Currency>();
+  const defaultCurrency = currencyData.find(item => item.CurrencyCode === "EUR");
 
-
-  const FindCurrencyDataByCode = (countryCode: string): Currency | undefined => {
+  const FindCurrencyDataByCode = (countryCode?: string): Currency | undefined => {
     return currencyData.find(item => item.CurrencyCode === countryCode);
   }
+
+
+  useEffect(() => {
+    const newCurrency = FindCurrencyDataByCode(props.countryCode);
+
+    setCurrency(newCurrency || defaultCurrency)
+  }, [defaultCurrency, props.countryCode]);
 
   return (
     <div className='currency-picker-box'>
       <button className='currency-picker'>
-        <div className="flag"><CountryFlag countryCode='GB' size={40}/></div>
+        <div className="flag"><CountryFlag countryCode={selectedCurrency?.Country} size={40}/></div>
         <div className="currency-info">
-          <div className="currency-code">EUR</div>
-          <div className="currency-name">euro</div>
+          <div className="currency-code">{selectedCurrency?.CurrencyCode}</div>
+          <div className="currency-name">{selectedCurrency?.CurrencyName}</div>
         </div>
       </button>
     </div>

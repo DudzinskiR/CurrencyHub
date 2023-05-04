d1c60cf470ecf44141980e6d38897d1126527b9f
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-04 18:06:13 +0200

    Add chart

--- frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
+++ frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
@@ -3,17 +3,24 @@ import "./CurrencyAnalysis.scss"
 import Header from '../../HeaderLine/HeaderLine'
 import CurrencyPicker from '../../CurrencyPicker/CurrencyPicker'
 import Button from '../../Button/Button'
+import { Bar } from 'react-chartjs-2';
+import "chart.js/auto";
+import { getData, options } from './ChartOptions'
 
 const CurrencyAnalysis = () => {
   const [currencyCode, setCurrencyCode] = useState<string>("USD");
+
 
   return (
     <div className="currency-analysis-box">
       <Header text='Analiza waluty' />
       <div className="currency-analysis-content">
+
         <div className="currency-analysis-chart-box">
-          s
+          <Bar options={options} data={getData([5, 25, 15])} />
         </div>
+
+
         <div className="currency-analysis-picker-box">
           <div className="currency-analysis-picker-title">Wybierz walute</div>
           <CurrencyPicker

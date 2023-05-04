463263141e640e5c46b254db2d4c4d85864646fc
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-04 16:01:10 +0200

    Create files

---
+++ frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CurrencyAnalysis.scss"
+
+const CurrencyAnalysis = () => {
+  return (
+    <div>CurrencyAnalysis</div>
+  )
+}
+
+export default CurrencyAnalysis

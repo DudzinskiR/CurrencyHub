501652bdd4f362b62a4424ccce78c99f77566d6e
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-04 16:37:55 +0200

    Add boxes and styles

--- frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
+++ frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./CurrencyAnalysis.scss"
+import Header from '../../HeaderLine/HeaderLine'
+import CurrencyPicker from '../../CurrencyPicker/CurrencyPicker'
 
 const CurrencyAnalysis = () => {
   return (
-    <div>CurrencyAnalysis</div>
+    <div className="currency-analysis-box">
+      <Header text='Analiza waluty' />
+      <div className="currency-analysis-content">
+        <div className="currency-analysis-chart-box">
+          s
+        </div>
+        <div className="currency-analysis-picker-box">
+          <div className="currency-analysis-picker-title">Wybierz walute</div>
+          <CurrencyPicker />
+        </div>
+      </div>
+    </div>
   )
 }
 

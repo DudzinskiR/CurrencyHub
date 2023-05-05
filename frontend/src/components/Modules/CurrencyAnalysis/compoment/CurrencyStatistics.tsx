409c3e8aed92706b735ac43c751c84093d3ca6c2
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 01:06:24 +0200

    Add loader to currency statistics component

--- frontend/src/components/Modules/CurrencyAnalysis/compoment/CurrencyStatistics.tsx
+++ frontend/src/components/Modules/CurrencyAnalysis/compoment/CurrencyStatistics.tsx
@@ -1,35 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./CurrencyStatistics.scss"
-
+import Loader from '../../../Loader/Loader'
 interface props {
   currencyCode: string,
   selectedTime: number
 }
 
 const CurrencyStatistics = ({ currencyCode, selectedTime }: props) => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isError, setError] = useState<boolean>(false);
+
+
   return (
     <div className='currency-statistics-box'>
-      <div className="currency-statistics-content">
-        <div className="currency-statistic">
-          <div className="currency-statistic-name">Mediana</div>
-          <div className="currency-statistic-value">12</div>
-        </div>
+      <Loader isLoading={isLoading} isError={isError}>
+        <div className="currency-statistics-content">
+          <div className="currency-statistic">
+            <div className="currency-statistic-name">Mediana</div>
+            <div className="currency-statistic-value">12</div>
+          </div>
 
-        <div className="currency-statistic">
-          <div className="currency-statistic-name">Dominanta</div>
-          <div className="currency-statistic-value">23</div>
-        </div>
+          <div className="currency-statistic">
+            <div className="currency-statistic-name">Dominanta</div>
+            <div className="currency-statistic-value">23</div>
+          </div>
 
-        <div className="currency-statistic">
-          <div className="currency-statistic-name">Odchylenie standardowe</div>
-          <div className="currency-statistic-value">34</div>
-        </div>
+          <div className="currency-statistic">
+            <div className="currency-statistic-name">Odchylenie standardowe</div>
+            <div className="currency-statistic-value">34</div>
+          </div>
 
-        <div className="currency-statistic">
-          <div className="currency-statistic-name">Współczynnik zmienności</div>
-          <div className="currency-statistic-value">45</div>
+          <div className="currency-statistic">
+            <div className="currency-statistic-name">Współczynnik zmienności</div>
+            <div className="currency-statistic-value">45</div>
+          </div>
         </div>
-      </div>
+      </Loader>
     </div>
   )
 }

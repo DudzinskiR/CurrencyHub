edf8ab864393c46dfcb0a047ea40701c368e916f
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 01:17:50 +0200

    Add currencyStatistics in ApiService

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -1,4 +1,5 @@
 import { CurrencyAnalysisData } from '../components/models/CurrencyAnalysisData';
+import { CurrencyStatisticData } from '../components/models/CurrencyStatisticsData';
 
 class ApiService {
   async currencyAnalysis(currencyCode: string): Promise<CurrencyAnalysisData[]> {
@@ -43,6 +44,53 @@ class ApiService {
         countUp: Math.random() * 50,
         countDown: Math.random() * 50,
         countConst: Math.random() * 50
+      },
+    ]
+  }
+
+  async currencyStatistics(currencyCode: string): Promise<CurrencyStatisticData[]>{
+    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
+
+    if(Math.random() * 10 > 8){
+      throw Error("Connection error")
+    }
+
+    return[
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
+      },
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
+      },
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
+      },
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
+      },
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
+      },
+      {
+        median: Math.random() * 50,
+        dominant: [Math.random() * 50, Math.random() * 50],
+        standardDeviation: Math.random() * 50,
+        coefficientOfVariantion: Math.random() * 50
       },
     ]
   }

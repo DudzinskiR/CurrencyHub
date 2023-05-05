42991a243d984280b1189cd806aaf03c74c4b3c1
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 00:58:42 +0200

    Add randomness to fetching currency analysis data

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -1,34 +1,49 @@
-import axios from 'axios';
+import { CurrencyAnalysisData } from '../components/models/CurrencyAnalysisData';
 
 class ApiService {
-  async currencyAnalysis() {
-    await new Promise(r => setTimeout(r, 1000));
+  async currencyAnalysis(currencyCode: string): Promise<CurrencyAnalysisData[]> {
+    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
 
+    if(Math.random() * 10 > 8){
+      throw Error("Connection error")
+    }
     return [
       {
         time: 7,
-        countUp: 1,
-        countDown: 2,
-        countConst: 3
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
       },
       {
         time: 14,
-        countUp: 1,
-        countDown: 2,
-        countConst: 3
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
       },
       {
         time: 30,
-        countUp: 1,
-        countDown: 2,
-        countConst: 3
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
       },
       {
         time: 90,
-        countUp: 1,
-        countDown: 2,
-        countConst: 3
-      }
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
+      },
+      {
+        time: 180,
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
+      },
+      {
+        time: 360,
+        countUp: Math.random() * 50,
+        countDown: Math.random() * 50,
+        countConst: Math.random() * 50
+      },
     ]
   }
 }

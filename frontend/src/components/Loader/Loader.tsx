0de90d0c31b032806c267e3a5b69c47a75085552
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-05 20:01:14 +0200

    Add isError prop in loader

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -3,17 +3,25 @@ import './Loader.scss'
 
 interface props {
   isLoading?: boolean,
+  isError?: boolean
   children?: React.ReactNode
 }
 
-const Loader = ({ isLoading, children }: props) => {
+const Loader = ({ isLoading, isError, children }: props) => {
   return (
     <div className='loader-box'>
-      {isLoading && <div className="loader-content">
+      {isLoading && !isError && <div className="loader-content">
         <div className="loader-text-background">
           <div className="loader-text">Ładowanie</div>
         </div>
       </div>}
+
+      {isError && <div className="loader-content">
+        <div className="loader-text-background">
+          <div className="loader-error-text">Błąd</div>
+        </div>
+      </div>}
+
       {children}
     </div>
   )

791c5fd8d8dfd54df5b5467bc33b84457f634227
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 12:45:16 +0200

    Add search icon in currencyPicker

--- frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
+++ frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
@@ -88,6 +88,15 @@ const CurrencyPicker = (props: props) => {
       {isOpen &&
         <div className='list-box'>
           <label>
+
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <g fill="rgb(100, 100, 100)">
+                  <path d="M 21 3 C 11.6 3 4 10.6 4 20 C 4 29.4 11.6 37 21 37 C 24.354553 37 27.47104 36.01984 30.103516 34.347656 L 42.378906 46.621094 L 46.621094 42.378906 L 34.523438 30.279297 C 36.695733 27.423994 38 23.870646 38 20 C 38 10.6 30.4 3 21 3 z M 21 7 C 28.2 7 34 12.8 34 20 C 34 27.2 28.2 33 21 33 C 13.8 33 8 27.2 8 20 C 8 12.8 13.8 7 21 7 z" />
+                </g>
+              </svg>
+            </div>
+
             <input
               placeholder="Wpisz walute..."
               className='currency-input'

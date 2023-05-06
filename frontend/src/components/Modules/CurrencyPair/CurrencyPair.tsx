e5cad6b1e33a994221222fde5539c0c281a4e157
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 14:35:27 +0200

    Add chart in currency pair

--- frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
+++ frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./CurrencyPair.scss"
+import { Bar } from 'react-chartjs-2';
+import "chart.js/auto";
 import Header from '../../HeaderLine/HeaderLine'
+import { getData, options } from './ChartOptions';
 
 const CurrencyPair = () => {
   return (
     <div className='currency-pair-box'>
       <Header text='Analiza par walut' />
+      <div className="currency-pair-content">
+        <div className="currency-pair-chart-box">
+          <Bar options={options} data={getData([10, 20, 30, 25, 15], ['label1', 'label2', 'label3', 'label4', 'label5'])} />
+        </div>
+      </div>
+
     </div>
   )
 }

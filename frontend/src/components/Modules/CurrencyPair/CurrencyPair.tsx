bdc3a5911e37cbead1fca7dd5a10c001f143d2dd
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 14:53:55 +0200

    Add timePicker to CurrencyPair

--- frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
+++ frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
@@ -3,15 +3,18 @@ import "./CurrencyPair.scss"
 import { Bar } from 'react-chartjs-2';
 import "chart.js/auto";
 import Header from '../../HeaderLine/HeaderLine'
-import { getData, options } from './ChartOptions';
+import { getData, options, timePeriodName } from './ChartOptions';
 import Loader from '../../Loader/Loader';
 import CurrencyPicker from '../../CurrencyPicker/CurrencyPicker';
 import Button from '../../Button/Button';
+import TimePicker from '../../TimePicker/TimePicker';
 
 const CurrencyPair = () => {
   const [currencyPair, setCurrencyPair] = useState<string[]>(['USD', 'GBP']);
   const [isLoading, setLoading] = useState<boolean>(false);
   const [isError, setError] = useState<boolean>(false);
+
+  const [selectedTime, setSelectedTime] = useState<number>(0);
 
   const updateCurrencyPair = (currencyCode: string, index: number) => {
     const newCurrencyPair = [...currencyPair];
@@ -25,6 +28,7 @@ const CurrencyPair = () => {
       <div className="currency-pair-content">
         <div className="currency-pair-chart-box">
           <Loader isLoading={isLoading} isError={isError}>
+            <TimePicker value={selectedTime} labels={timePeriodName} onChange={(value) => setSelectedTime(value)} />
             <Bar options={options} data={getData([10, 20, 30, 25, 15], ['label1', 'label2', 'label3', 'label4', 'label5'])} />
           </Loader>
         </div>

d00431aaea61973767e097fe1b1a7044916d6dab
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 15:10:41 +0200

    Add currencyPair in apiService

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -1,7 +1,87 @@
 import { CurrencyAnalysisData } from '../components/models/CurrencyAnalysisData';
+import { CurrencyPairData } from '../components/models/CurrencyPairData';
 import { CurrencyStatisticData } from '../components/models/CurrencyStatisticsData';
 
 class ApiService {
+
+  async currencyPair(firstCurrencyCode: string, secondCurrencyCode: string): Promise<CurrencyPairData[][]>{
+    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
+
+    if(Math.random() * 10 > 8){
+      throw Error("Connection error")
+    }
+
+    const temp = Math.random() * 2;
+    const temp2 = Math.random() * 2;
+
+    return[
+      [
+        {
+          time: 30,
+          startScope: `${(temp * -1)}`,
+          endScope: `${(temp * -1) + (temp * 0.4)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp * -1) + (temp * 0.4)}`,
+          endScope: `${(temp * -1) + (temp * 0.8)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp * -1) + (temp * 0.8)}`,
+          endScope: `${(temp * -1) + (temp * 1.2)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp * -1) + (temp * 1.2)}`,
+          endScope: `${(temp * -1) + (temp * 1.6)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp * -1) + (temp * 1.6)}`,
+          endScope: `${(temp * -1) + (temp * 2)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+      ],
+      [
+        {
+          time: 30,
+          startScope: `${(temp2 * -1)}`,
+          endScope: `${(temp2 * -1) + (temp2 * 0.4)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp2 * -1) + (temp2 * 0.4)}`,
+          endScope: `${(temp2 * -1) + (temp2 * 0.8)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp2 * -1) + (temp2 * 0.8)}`,
+          endScope: `${(temp2 * -1) + (temp2 * 1.2)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp2 * -1) + (temp2 * 1.2)}`,
+          endScope: `${(temp2 * -1) + (temp2 * 1.6)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+        {
+          time: 30,
+          startScope: `${(temp2 * -1) + (temp2 * 1.6)}`,
+          endScope: `${(temp2 * -1) + (temp2 * 2)}`,
+          value: Math.floor(Math.random() * 20)
+        },
+      ]
+    ]
+  }
+
   async currencyAnalysis(currencyCode: string): Promise<CurrencyAnalysisData[]> {
     await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
 

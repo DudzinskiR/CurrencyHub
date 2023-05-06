ef0a5c20bbfdfe6a547f1e2ad2baab39a8dafee5
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 14:43:40 +0200

    Add currency picker box in currency pair

--- frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
+++ frontend/src/components/Modules/CurrencyPair/CurrencyPair.tsx
@@ -5,6 +5,8 @@ import "chart.js/auto";
 import Header from '../../HeaderLine/HeaderLine'
 import { getData, options } from './ChartOptions';
 import Loader from '../../Loader/Loader';
+import CurrencyPicker from '../../CurrencyPicker/CurrencyPicker';
+import Button from '../../Button/Button';
 
 const CurrencyPair = () => {
   const [isLoading, setLoading] = useState<boolean>(false);
@@ -19,8 +21,16 @@ const CurrencyPair = () => {
             <Bar options={options} data={getData([10, 20, 30, 25, 15], ['label1', 'label2', 'label3', 'label4', 'label5'])} />
           </Loader>
         </div>
-      </div>
 
+        <div className="currency-pair-picker-box">
+          <div className="currency-pair-picker-title">Wybierz waluty</div>
+          <CurrencyPicker />
+          <div className='currency-pair-picker-and'>-oraz-</div>
+          <CurrencyPicker />
+          <Button text='Wybierz' />
+        </div>
+
+      </div>
     </div>
   )
 }

2e2f7fcd59d53626ef25714d0b7740e57f5b1a28
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-06 16:37:47 +0200

    Update currencyPair in ApiService

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -4,81 +4,81 @@ import { CurrencyStatisticData } from '../components/models/CurrencyStatisticsDa
 
 class ApiService {
 
-  async currencyPair(firstCurrencyCode: string, secondCurrencyCode: string): Promise<CurrencyPairData[][]>{
+  async currencyPair(firstCurrencyCode: string, secondCurrencyCode: string): Promise<CurrencyPairData[]>{
     await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
 
     if(Math.random() * 10 > 8){
       throw Error("Connection error")
     }
 
-    const temp = Math.random() * 2;
-    const temp2 = Math.random() * 2;
+    const temp = Math.random() * 1;
+    const temp2 = Math.random() * 1;
 
-    return[
-      [
-        {
-          time: 30,
-          startScope: `${(temp * -1)}`,
-          endScope: `${(temp * -1) + (temp * 0.4)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp * -1) + (temp * 0.4)}`,
-          endScope: `${(temp * -1) + (temp * 0.8)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp * -1) + (temp * 0.8)}`,
-          endScope: `${(temp * -1) + (temp * 1.2)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp * -1) + (temp * 1.2)}`,
-          endScope: `${(temp * -1) + (temp * 1.6)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp * -1) + (temp * 1.6)}`,
-          endScope: `${(temp * -1) + (temp * 2)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-      ],
-      [
-        {
-          time: 30,
-          startScope: `${(temp2 * -1)}`,
-          endScope: `${(temp2 * -1) + (temp2 * 0.4)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp2 * -1) + (temp2 * 0.4)}`,
-          endScope: `${(temp2 * -1) + (temp2 * 0.8)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp2 * -1) + (temp2 * 0.8)}`,
-          endScope: `${(temp2 * -1) + (temp2 * 1.2)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp2 * -1) + (temp2 * 1.2)}`,
-          endScope: `${(temp2 * -1) + (temp2 * 1.6)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-        {
-          time: 30,
-          startScope: `${(temp2 * -1) + (temp2 * 1.6)}`,
-          endScope: `${(temp2 * -1) + (temp2 * 2)}`,
-          value: Math.floor(Math.random() * 20)
-        },
-      ]
+    return [
+      {
+        time: 30,
+        scopes: [
+          {
+            startScope: (temp * -1),
+            endScope: (temp * -1) + (temp * 0.4),
+          },
+          {
+            startScope: (temp * -1) + (temp * 0.4),
+            endScope: (temp * -1) + (temp * 0.8),
+          },
+          {
+            startScope: (temp * -1) + (temp * 0.8),
+            endScope: (temp * -1) + (temp * 1.2),
+          },
+          {
+            startScope: (temp * -1) + (temp * 1.2),
+            endScope: (temp * -1) + (temp * 1.6),
+          },
+          {
+            startScope: (temp * -1) + (temp * 1.6),
+            endScope: (temp * -1) + (temp * 2),
+          },
+        ],
+        values: [
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20)
+        ]
+      },
+      {
+        time: 30,
+        scopes: [
+          {
+            startScope: (temp2 * -1),
+            endScope: (temp2 * -1) + (temp2 * 0.4),
+          },
+          {
+            startScope: (temp2 * -1) + (temp2 * 0.4),
+            endScope: (temp2 * -1) + (temp2 * 0.8),
+          },
+          {
+            startScope: (temp2 * -1) + (temp2 * 0.8),
+            endScope: (temp2 * -1) + (temp2 * 1.2),
+          },
+          {
+            startScope: (temp2 * -1) + (temp2 * 1.2),
+            endScope: (temp2 * -1) + (temp2 * 1.6),
+          },
+          {
+            startScope: (temp2 * -1) + (temp2 * 1.6),
+            endScope: (temp2 * -1) + (temp2 * 2),
+          },
+        ],
+        values: [
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20),
+          Math.floor(Math.random() * 20)
+        ]
+      },
     ]
   }
 

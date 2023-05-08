6e975cfd2ed2787b6c32679c267c129c5cde981b
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-08 02:16:47 +0200

    Add new table to migration

---
+++ backend/src/db/migrations/20230508001529_init.js
@@ -0,0 +1,26 @@
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('currency_refresh', table => {
+    table.increments('id').primary();
+    table.string('code').notNullable();
+    table.date('time').notNullable();
+    table.timestamps(true, true);
+  }).createTable('currency_rates', table => {
+    table.increments('id').primary();
+    table.string('code').notNullable();
+    table.date('time').notNullable();
+    table.float('value').notNullable()
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('currency_refresh').dropTable('currency_rates');
+};

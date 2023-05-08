1699560350070ef2a5c6a87e7c21aa89a3f791fc
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-08 04:26:41 +0200

    Add data fetchning from api.nbp.pl

--- backend/src/modules/analysis/analysis.service.ts
+++ backend/src/modules/analysis/analysis.service.ts
@@ -3,16 +3,7 @@ import CurrencyRefreshService from '../currency-refresh/currency-refresh.service
 
 class AnalysisService {
   static async getAnalysis(currencyCode: string) {
-    const isOld = await CurrencyRefreshService.isDataOld(currencyCode);
-
-    if(isOld) {
-
-    } else {
-      const temp = await AnalysisModel.getAnalysis(currencyCode);
-    }
-    
-    // CommonService.isDataOld('USD')
-    // return AnalysisModel.getAnalysis(req, res)
+    await CurrencyRefreshService.refreshCurrencyData(currencyCode);
   }
 }
 
--- backend/src/modules/currency-refresh/currency-refresh.model.ts
+++ backend/src/modules/currency-refresh/currency-refresh.model.ts
@@ -3,11 +3,45 @@ import knex from 'knex';
 import db from '../../db/db'
 import { CurrencyRefreshData } from '../../interfaces/currency-refresh';
 import { TABLE_NAME } from '../common/common.enum';
+import { CurrencyRate } from '../../interfaces/currency-rate'
+import axios from 'axios';
 
 class CurrencyRefreshModel{
   static async getLastCurrencyRefresh(currencyCode: string): Promise<CurrencyRefreshData> {
     const res = await db(TABLE_NAME.CURRENCY_REFRESH).select('time').where('code', currencyCode);
     return res[0];
+  }
+
+  static async getCurrencyRates(currencyCode: string, startDate: Date, endDate: Date): Promise<CurrencyRate[]> {
+    const rates: CurrencyRate[] = []
+
+    try {
+      const url = `http://api.nbp.pl/api/exchangerates/rates/A/${currencyCode}/${startDate.toISOString().split('T')[0]}/${endDate.toISOString().split('T')[0]}?format=json`;
+      const response = await axios.get(url);
+      for(const item of response.data.rates){
+        rates.push({
+          code: currencyCode,
+          time: new Date(item.effectiveDate),
+          value: item.mid
+        })
+      }
+      
+    } catch (err){
+
+    }
+    return rates;
+  }
+
+  static async createNewRefresh(data: CurrencyRefreshData) {
+    await db(TABLE_NAME.CURRENCY_REFRESH).insert(data);
+  }
+
+  static async updateRefresh(data: CurrencyRefreshData) {
+    await db(TABLE_NAME.CURRENCY_REFRESH).where({code: data.code}).update(data)
+  }
+
+  static async createNewRates(rates: CurrencyRate[]){
+    await db(TABLE_NAME.CURRENCY_RATES).insert(rates);
   }
 }
 

d1bf973bc393485067445d1e568cb26bde4fe7f1
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-09 17:21:37 +0200

    feat(statistics): get currency data from db

--- backend/src/modules/statistics/statistics.model.ts
+++ backend/src/modules/statistics/statistics.model.ts
@@ -1,5 +1,21 @@
-class StatisticsModel {
+import db from "../../db/db";
+import DatabaseError from "../../exceptions/database-error.exception";
+import { CurrencyStatisticsData } from "../../interfaces/currency-statistics-data";
+import { TABLE_NAME } from "../common/common.enum";
 
+class StatisticsModel {
+  static async getStatisticsDesc(currencyCode: string){
+    try {
+      const today = new Date();
+      const res: CurrencyStatisticsData[] = await db(TABLE_NAME.CURRENCY_RATES)
+        .select("time").select("value")
+        .where({code: currencyCode}).where("time", ">", new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()))
+        .orderBy('time', 'desc');
+      return res;
+    } catch(e) {
+      throw new DatabaseError();
+    }
+  }
 }
 
 export default StatisticsModel;

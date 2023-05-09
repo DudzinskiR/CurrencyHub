1178dcf7146ebad8f498693b5083c33ba7030fcd
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-09 17:37:37 +0200

    feat(statistics): start working on service

--- backend/src/modules/statistics/statistics.service.ts
+++ backend/src/modules/statistics/statistics.service.ts
@@ -1,5 +1,20 @@
+import { validateCode } from "../../common/currency-validator/currency-validator";
+import InvalidCurrencyError from "../../exceptions/invalid-currency.exception";
+import AnalysisModel from "../analysis/analysis.model";
+import CurrencyRefreshService from "../currency-refresh/currency-refresh.service";
+
 class StatisticsService {
   static async getStatistics(currencyCode: string){
+    await CurrencyRefreshService.refreshCurrencyData(currencyCode);
+    
+    if(!validateCode(currencyCode)){
+      throw new InvalidCurrencyError(currencyCode);
+    }
+
+    const response = await AnalysisModel.getAnalysisDesc(currencyCode);
+    console.log("🚀 ~ file: statistics.service.ts:15 ~ StatisticsService ~ getStatistics ~ response:", response)
+
+    
     
   }
 }

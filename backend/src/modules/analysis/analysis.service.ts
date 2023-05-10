2edc974edcdc90fd0de04fad8b6f57f8db60f027
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-10 03:48:26 +0200

    feat(analysis): use DateProcessor to process list of dates

--- backend/src/modules/analysis/analysis.service.ts
+++ backend/src/modules/analysis/analysis.service.ts
@@ -3,6 +3,8 @@ import CurrencyRefreshService from '../currency-refresh/currency-refresh.service
 import { getTableByCode, validateCode } from '../../common/currency-validator/currency-validator';
 import InvalidCurrencyError from '../../exceptions/invalid-currency.exception';
 import { currencyAnalysis } from '../../interfaces/currency-analysis';
+import DateProcessor, { DateInfo } from '../../common/date-processor/date-processor';
+import { timeBreakpoints } from '../../common/const';
 
 class AnalysisService {
   static async getAnalysis(currencyCode: string) {
@@ -14,50 +16,29 @@ class AnalysisService {
 
     const data = await AnalysisModel.getAnalysisDesc(currencyCode)
     
-    const result: currencyAnalysis[] = [
-      { up: 0, const: 0, down: 0 },
-      { up: 0, const: 0, down: 0 },
-      { up: 0, const: 0, down: 0 },
-      { up: 0, const: 0, down: 0 },
-      { up: 0, const: 0, down: 0 },
-      { up: 0, const: 0, down: 0 },
-    ];
-    
-    const timeBreakpoint = [7, 14, 30, 90, 180, 9999];
-    let timeIndex = 0;
-    
-    const daysDiff = (start: Date, end: Date) => {
-      return Math.floor((start.getTime() - end.getTime()) / (1000 * 60 * 60 * 24))
-    };
-    
-    for(let i = 1; i < data.length; i++){
-      if(daysDiff(new Date(), data[i].time) > timeBreakpoint[timeIndex]) {
-        timeIndex++;
-      }
-      
-      const valueDiff = data[i].value - data[i - 1].value;
+    const dates: DateInfo[] = [];
+    data.forEach(item => {
+      dates.push({date: item.time, value:item.value});
+    })
 
-      if(valueDiff > 0) {
-        result[timeIndex].up++;
-      } else if(valueDiff === 0) {
-        result[timeIndex].const++;
-      } else {
-        result[timeIndex].down++;
-      }
-    }
-    const output: currencyAnalysis[] = [];
+    const dateProcessor = new DateProcessor<currencyAnalysis>().setDates(dates).setTimeBreakpoints(timeBreakpoints);
+    dateProcessor.setCallback((tab: DateInfo[]): currencyAnalysis => {
+      const result = { up: 0, const: 0, down: 0 };
 
-    for(let i = 0; i < result.length; i++) {
-      const temp: currencyAnalysis = {up: 0, const: 0, down: 0};
-      for(let j = 0; i >= j; j++){
-        temp.up += result[j].up;
-        temp.const += result[j].const;
-        temp.down += result[j].down;
-      }
-      output.push(temp);
-    }
+      for(let i = 1; i < tab.length; i++){
+        const valueDiff = data[i].value - data[i - 1].value;
 
-    return output;
+        if(valueDiff > 0) {
+          result.up++;
+        } else if(valueDiff === 0) {
+          result.const++;
+        } else {
+          result.down++;
+        }
+      }
+      return result;
+    });
+    return dateProcessor.execute();
   }
 }
 

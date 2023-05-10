ebcf3c18d7af50696910c95316014e1ccd49467a
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-10 14:11:50 +0200

    feat(statistics): Create tests to helpers

---
+++ backend/src/modules/statistics/__test__/helper/deviation.test.ts
@@ -0,0 +1,32 @@
+import { DateInfo } from "../../../../common/date-processor/date-processor";
+import calcStandardDeviation from "../../helper/deviation";
+
+describe('calCDeviation', () => {
+  it('should return 0 for empty input list', () => {
+    const mockList: DateInfo[] = [];
+    const result = calcStandardDeviation(mockList);
+    expect(result).toEqual(0);
+  });
+
+  it('should calculate standard deviation correctly', () => {
+    const mockList: DateInfo[] = [
+      { date: new Date(), value: 10 },
+      { date: new Date(), value: 20 },
+      { date: new Date(), value: 30 },
+      { date: new Date(), value: 40 },
+      { date: new Date(), value: 50 },
+    ];
+
+    const result = calcStandardDeviation(mockList);
+    expect(result).toBeCloseTo(15.8113883, 6);
+  });
+
+  it('should return 0 for a single-value list', () => {
+    const mockList: DateInfo[] = [
+      { date: new Date(), value: 10 }
+    ];
+
+    const result = calcStandardDeviation(mockList);
+    expect(result).toEqual(0);
+  })
+})

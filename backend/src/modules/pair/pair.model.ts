b920275e637521542b76ed370730a6b900c26374
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-11 01:48:39 +0200

    feat(pair): finish work on pair endpoint

--- backend/src/modules/pair/pair.model.ts
+++ backend/src/modules/pair/pair.model.ts
@@ -1,8 +1,21 @@
+import db from "../../db/db";
+import DatabaseError from "../../exceptions/database-error.exception";
+import { CurrencyAnalysisData } from "../../interfaces/currency-analysis-data";
 import { CurrencyPairData } from "../../interfaces/currency-pair-data";
+import { TABLE_NAME } from "../common/common.enum";
 
 class PairModel {
   static async getCurrencyDataDesc(code: string): Promise<CurrencyPairData[]>{
-    return [];
+    try {  
+      const today = new Date();
+      const res: CurrencyAnalysisData[] = await db(TABLE_NAME.CURRENCY_RATES)
+        .select("time").select("value")
+        .where({code: code}).where("time", ">", new Date(today.getFullYear(), today.getMonth() - 3, today.getDate()))
+        .orderBy('time', 'desc');
+      return res;
+    } catch (e){
+      throw new DatabaseError();
+    }
   }
 }
 

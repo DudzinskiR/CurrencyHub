ebc488ba6e43cf8c4353cc2f51b1ac9759332b5d
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-10 14:14:58 +0200

    feat(statistics): use DateProcessor to process date

--- backend/src/modules/statistics/statistics.service.ts
+++ backend/src/modules/statistics/statistics.service.ts
@@ -1,8 +1,11 @@
+import { timeBreakpoints } from "../../common/const";
 import { validateCode } from "../../common/currency-validator/currency-validator";
+import DateProcessor, { DateInfo } from "../../common/date-processor/date-processor";
 import InvalidCurrencyError from "../../exceptions/invalid-currency.exception";
+import { CurrencyStatistic } from "../../interfaces/currency-statistics";
 import AnalysisModel from "../analysis/analysis.model";
 import CurrencyRefreshService from "../currency-refresh/currency-refresh.service";
-
+import {calcStandardDeviation, calcDominant, calcMedian, calcCoefficientOfVariation} from './helper'
 class StatisticsService {
   static async getStatistics(currencyCode: string){
     await CurrencyRefreshService.refreshCurrencyData(currencyCode);
@@ -11,11 +14,28 @@ class StatisticsService {
       throw new InvalidCurrencyError(currencyCode);
     }
 
-    const response = await AnalysisModel.getAnalysisDesc(currencyCode);
-    console.log("🚀 ~ file: statistics.service.ts:15 ~ StatisticsService ~ getStatistics ~ response:", response)
+    const data = await AnalysisModel.getAnalysisDesc(currencyCode);
+    
+    const dates: DateInfo[] = [];
+    data.forEach(item => {
+      dates.push({date: item.time, value:item.value});
+    });
 
-    
-    
+    const dateProcessor = new DateProcessor<CurrencyStatistic>()
+      .setDates(dates)
+      .setTimeBreakpoints(timeBreakpoints)
+      .setCallback((tab: DateInfo[]): CurrencyStatistic => {
+        const result: CurrencyStatistic = {
+          median: calcMedian(tab),
+          dominant: calcDominant(tab),
+          deviation: calcStandardDeviation(tab),
+          variation: calcCoefficientOfVariation(tab)
+        }
+        return result;
+      })
+
+    const output = dateProcessor.execute();
+
   }
 }
 

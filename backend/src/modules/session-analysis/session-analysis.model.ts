c9cbaf063c541732c31e34efc7faa5e66fd1d520
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-11 04:07:30 +0200

    chore: fix name

--- backend/src/modules/session-analysis/session-analysis.model.ts
+++ backend/src/modules/session-analysis/session-analysis.model.ts
@@ -1,19 +1,19 @@
 import db from '../../db/db'
-import DatabaseError from '../../exceptions/database-error.exception';
+import DatabaseException from '../../exceptions/database-error.exception';
 import { TABLE_NAME } from '../../common/table-name.enum';
 import { CurrencyRate } from '../../interfaces/currency-rate';
 
 class SessionAnalysisModel{
-  static async getSessionAnalysisDesc(currencyCode: string): Promise<CurrencyRate[]> {
+  static async getSessionAnalysisDesc(code: string): Promise<CurrencyRate[]> {
     try{
       const today = new Date();
       const res: CurrencyRate[] = await db(TABLE_NAME.CURRENCY_RATES)
         .select("time").select("value")
-        .where({code: currencyCode}).where("time", ">", new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()))
+        .where({code: code}).where("time", ">", new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()))
         .orderBy('time', 'desc');
       return res;
     } catch (e){
-      throw new DatabaseError();
+      throw new DatabaseException();
     }
   }
 }
--- backend/src/modules/session-analysis/session-analysis.service.ts
+++ backend/src/modules/session-analysis/session-analysis.service.ts
@@ -1,20 +1,20 @@
 import SessionAnalysisModel from './session-analysis.model';
 import CurrencyRefreshService from '../currency-refresh/currency-refresh.service';
-import { getTableByCode, validateCode } from '../../common/currency-validator/currency-validator';
+import { validateCode } from '../../common/currency-validator/currency-validator';
 import InvalidCurrencyException from '../../exceptions/invalid-currency.exception';
 import { SessionAnalysis } from './session-analysis.interface';
 import DateProcessor, { DateInfo } from '../../common/date-processor/date-processor';
 import { timeBreakpoints } from '../../common/const';
 
 class SessionAnalysisService {
-  static async getSessionAnalysis(currencyCode: string): Promise<SessionAnalysis[]> {
-    await CurrencyRefreshService.refreshCurrencyData(currencyCode);
+  static async getSessionAnalysis(code: string): Promise<SessionAnalysis[]> {
+    await CurrencyRefreshService.refreshCurrencyData(code);
 
-    if(!validateCode(currencyCode)){
-      throw new InvalidCurrencyException(currencyCode);
+    if(!validateCode(code)){
+      throw new InvalidCurrencyException(code);
     }
 
-    const data = await SessionAnalysisModel.getSessionAnalysisDesc(currencyCode)
+    const data = await SessionAnalysisModel.getSessionAnalysisDesc(code)
     
     const dates: DateInfo[] = [];
     data.forEach(item => {

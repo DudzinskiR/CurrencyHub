c828800702d7f9730e2faf4b6cb9989662847034
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-11 17:50:49 +0200

    feat: send requests to backend

--- frontend/src/services/backend.service.ts
+++ frontend/src/services/backend.service.ts
@@ -1,169 +1,28 @@
 import { SessionAnalysisData } from '../models/session-analysis.interface';
 import { ChangeDistributionData } from '../models/change-distribution.interface';
 import { StatisticData } from '../models/statistics.interface';
+import axios from 'axios';
 
 class BackendService {
-  static async getChangeDistribution(firstCurrencyCode: string, secondCurrencyCode: string): Promise<ChangeDistributionData[]>{
-    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
+  static async getChangeDistribution(codeOne: string, codeTwo: string): Promise<ChangeDistributionData[]>{
+    const url = `${process.env.REACT_APP_API_ROOT}/api/change/?one=${codeOne}&two=${codeTwo}`;
+    const response = await axios.get<ChangeDistributionData[]>(url);
 
-    if(Math.random() * 10 > 8){
-      throw Error("Connection error")
-    }
-
-    const temp = Math.random() * 1;
-    const temp2 = Math.random() * 1;
-
-    return [
-      {
-        scopes: [
-          {
-            start: (temp * -1),
-            end: (temp * -1) + (temp * 0.4),
-          },
-          {
-            start: (temp * -1) + (temp * 0.4),
-            end: (temp * -1) + (temp * 0.8),
-          },
-          {
-            start: (temp * -1) + (temp * 0.8),
-            end: (temp * -1) + (temp * 1.2),
-          },
-          {
-            start: (temp * -1) + (temp * 1.2),
-            end: (temp * -1) + (temp * 1.6),
-          },
-          {
-            start: (temp * -1) + (temp * 1.6),
-            end: (temp * -1) + (temp * 2),
-          },
-        ],
-        values: [
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20)
-        ]
-      },
-      {
-        scopes: [
-          {
-            start: (temp2 * -1),
-            end: (temp2 * -1) + (temp2 * 0.4),
-          },
-          {
-            start: (temp2 * -1) + (temp2 * 0.4),
-            end: (temp2 * -1) + (temp2 * 0.8),
-          },
-          {
-            start: (temp2 * -1) + (temp2 * 0.8),
-            end: (temp2 * -1) + (temp2 * 1.2),
-          },
-          {
-            start: (temp2 * -1) + (temp2 * 1.2),
-            end: (temp2 * -1) + (temp2 * 1.6),
-          },
-          {
-            start: (temp2 * -1) + (temp2 * 1.6),
-            end: (temp2 * -1) + (temp2 * 2),
-          },
-        ],
-        values: [
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20),
-          Math.floor(Math.random() * 20)
-        ]
-      },
-    ]
+    return response.data;
   }
 
-  static async getSessionAnalysis(currencyCode: string): Promise<SessionAnalysisData[]> {
-    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
-
-    if(Math.random() * 10 > 8){
-      throw Error("Connection error")
-    }
-    return [
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-      {
-        up: Math.random() * 50,
-        down: Math.random() * 50,
-        const: Math.random() * 50
-      },
-    ]
+  static async getSessionAnalysis(code: string): Promise<SessionAnalysisData[]> {
+    const url = `${process.env.REACT_APP_API_ROOT}/api/session/?code=${code}`;
+    const response = await axios.get<SessionAnalysisData[]>(url);
+    
+    return response.data;
   }
 
-  static async getStatistics(currencyCode: string): Promise<StatisticData[]>{
-    await new Promise(r => setTimeout(r, 1000 + Math.random() * 2000));
-
-    if(Math.random() * 10 > 8){
-      throw Error("Connection error")
-    }
-
-    return[
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-      {
-        median: Math.random() * 50,
-        dominant: [Math.random() * 50, Math.random() * 50],
-        deviation: Math.random() * 50,
-        variation: Math.random() * 50
-      },
-    ]
+  static async getStatistics(code: string): Promise<StatisticData[]>{
+    const url = `${process.env.REACT_APP_API_ROOT}/api/statistics/?code=${code}`;
+    const response = await axios.get<StatisticData[]>(url);
+    
+    return response.data;
   }
 }
 

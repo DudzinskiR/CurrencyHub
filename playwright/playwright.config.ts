4807c7ac2bb9ae2abfc7d5abee379eff6b422000
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-13 01:20:55 +0200

    test(playwright): add outputDir and reduce devices

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -34,19 +34,24 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'Desktop Chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'Desktop Firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'Desktop Webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5']}
+    // }
 
     /* Test against mobile viewports. */
     // {
@@ -68,7 +73,7 @@ export default defineConfig({
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+  outputDir: 'test-results/',
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',

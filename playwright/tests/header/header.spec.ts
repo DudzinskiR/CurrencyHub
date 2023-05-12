3c8fb3c52433e31bff748007f5f916f4b3e0ed4f
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-05-13 01:23:31 +0200

    test(playwright): add tests for header

---
+++ playwright/tests/header/header.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({page}) => {
+  await page.goto(`${process.env.ROOT}`);
+  await page.waitForLoadState('domcontentloaded');
+})
+
+test.describe('Header', () => {
+  test('should contain "Walutownik" text', async ({ page }, testInfo) => {
+    const text = await page.getByText('Walutownik');
+
+    await expect(page.locator('.header-bar-2')).toHaveScreenshot('header-bar.png')
+    await expect(text).toBeVisible();
+    await testInfo.attach('screenshot', { body: await page.locator('.header-bar-2').screenshot(), contentType: 'image/png' });
+  });
+});
